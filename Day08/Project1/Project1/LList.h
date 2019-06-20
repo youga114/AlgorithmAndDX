75453d7de881b9fe9e1717c01ac442796c6ff7d5
youga114 <youga114@naver.com>
2019-06-20 17:57:15 +0900

    Day09 (DLList 구현중)

--- Day08/Project1/Project1/LList.h
+++ Day08/Project1/Project1/LList.h
@@ -1,14 +1,24 @@
 #pragma once
-
+//------------
+//|  |  1  | |
+//------------
+//------------
+//|  |  2  | |
+//------------
+//------------
+//|  |  3  | |
+//------------
+template<typename T>
 class Node
 {
 public:
-	int Value;
+	T Value;
 
-	Node* Prev;
-	Node* Next;
+	Node<T>* Prev;
+	Node<T>* Next;
 };
 
+template<typename T>
 class LList
 {
 public:
@@ -18,35 +28,195 @@ public:
 	class Iterator
 	{
 	private:
-		Node* Current;
+		Node<T>* Current;
 	public:
-		Iterator(Node* init = nullptr) : Current(init) {}
+		Iterator(Node<T>* init = nullptr) : Current(init) {}
 		Iterator& operator++()
 		{
 			Current = Current->Next;
 			return *this;
 		}
 
-		Node* operator*()
+		Iterator operator++(int)
+		{
+			Node<T> Temp = *Current;
+			Current = Current->Next;
+			return Iterator(&Temp);
+		}
+
+		Node<T>* operator* ()
 		{
 			return Current;
 		}
 
-		bool operator!=(const Iterator& i)
+		bool operator!=(const Iterator& rhs)
 		{
-			return (Current != i.Current);
+			return (Current != rhs.Current);
+		}
+
+		bool operator==(const Iterator& rhs)
+		{
+			return (Current == rhs.Current);
 		}
 	};
 
-	void Insert(int NewValue);
-	void Insert(int NewValue, int SearchValue);
-	void Remove(int RemoveValue);
-	Iterator Find(int SearchValue);
+	class ReverseIterator
+	{
+	private:
+		Node<T>* Current;
+	public:
+		ReverseIterator(Node<T>* init = nullptr) : Current(init) {}
+		ReverseIterator& operator++()
+		{
+			Current = Current->Prev;
+			return *this;
+		}
 
-	Iterator Begin() { return Iterator(Head->Next); };
-	Iterator End() { return Iterator(Tail); };
+		ReverseIterator operator++(int)
+		{
+			Node<T> Temp = *Current;
+			Current = Current->Prev;
+			return ReverseIterator(&Temp);
+		}
+
+		Node<T>* operator* ()
+		{
+			return Current;
+		}
+
+		bool operator!=(const ReverseIterator& rhs)
+		{
+			return (Current != rhs.Current);
+		}
+
+		bool operator==(const ReverseIterator& rhs)
+		{
+			return (Current == rhs.Current);
+		}
+	};
+
+	void PushBack(T NewValue);
+	void PushFront(T NewValue);
+
+	void InsertAfter(LList::Iterator Where, T SearchValue);
+	void InsertBefore(LList::Iterator Where, T SearchValue);
+	Iterator Find(T SearchValue);
+
+	Iterator Begin() { return Iterator(Head->Next); }
+	Iterator End() { return Iterator(Tail); }
+
+	ReverseIterator RBegin() { return ReverseIterator(Tail->Prev); }
+	ReverseIterator REnd() { return ReverseIterator(Head); }
+
+	Iterator Erase(Iterator Where)
+	{
+		(*Where)->Prev->Next = (*Where)->Next;
+		(*Where)->Next->Prev = (*Where)->Prev;
+		Iterator Return = Iterator((*Where)->Next);
+		delete (*Where);
+
+		return Return;
+	}
 
 protected:
-	Node* Tail;
-	Node* Head;
+	Node<T>* Tail;
+	Node<T>* Head;
+
+	Node<T>* MakeNode(T NewValue);
 };
+
+template<typename T>
+LList<T>::LList()
+{
+	Head = new Node<T>();
+	Tail = new Node<T>();
+	Head->Next = Tail;
+	Tail->Prev = Head;
+}
+
+
+template<typename T>
+LList<T>::~LList()
+{
+}
+
+template<typename T>
+void LList<T>::PushBack(T NewValue)
+{
+	Node<T>* NewNode = MakeNode(NewValue);
+
+	if (NewNode)
+	{
+		NewNode->Prev = Tail->Prev;
+		Tail->Prev = NewNode;
+		NewNode->Next = Tail;
+		NewNode->Prev->Next = NewNode;
+	}
+}
+
+template<typename T>
+void LList<T>::PushFront(T NewValue)
+{
+	Node<T>* NewNode = MakeNode(NewValue);
+
+	if (NewNode)
+	{
+		NewNode->Prev = Head;
+		NewNode->Next = Head->Next;
+		NewNode->Next->Prev = NewNode;
+		Head->Next = NewNode;
+	}
+}
+
+template<typename T>
+void LList<T>::InsertAfter(LList<T>::Iterator Where, T NewValue)
+{
+	if (Where != nullptr)
+	{
+		Node<T>* NewNode = MakeNode(NewValue);
+
+
+		NewNode->Next = (*Where)->Next;
+		NewNode->Prev = (*Where);
+		(*Where)->Next = NewNode;
+		NewNode->Next->Prev = NewNode;
+	}
+}
+
+template<typename T>
+void LList<T>::InsertBefore(LList<T>::Iterator Where, T NewValue)
+{
+	if (Where != nullptr)
+	{
+		Node<T>* NewNode = MakeNode(NewValue);
+
+
+		NewNode->Next = (*Where);
+		NewNode->Prev = (*Where)->Prev;
+		(*Where)->Prev = NewNode;
+		NewNode->Prev->Next = NewNode;
+	}
+}
+
+template<typename T>
+typename LList<T>::Iterator LList<T>::Find(T SearchValue)
+{
+	for (LList<T>::Iterator Current = Begin(); Current != End(); ++Current)
+	{
+		if ((*Current)->Value == SearchValue)
+		{
+			return Current;
+		}
+	}
+
+	return nullptr;
+}
+
+template<typename T>
+Node<T> * LList<T>::MakeNode(T NewValue)
+{
+	Node<T>* NewNode = new Node<T>();
+	NewNode->Value = NewValue;
+
+	return NewNode;
+}
--- Day08/Project1/Project1/main.cpp
+++ Day08/Project1/Project1/main.cpp
@@ -1,24 +1,61 @@
 #include <iostream>
 #include <list>
+#include <vector>
 #include "LList.h"
+
 using namespace std;
 
 int main()
 {
-	LList l;
-	for (int i = 0; i < 10; ++i)
+	LList<int> l;
+	for (int i = 1; i <= 10; ++i)
 	{
-		l.Insert(i);
+		l.PushBack(i);
 	}
 
-	l.Insert(20, 3);
-	l.Insert(30, 20);
-	l.Insert(40, 9);
+	for (LList<int>::Iterator CurrentPosition = l.Begin(); CurrentPosition != l.End(); CurrentPosition++)
+	{
+		l.Erase(CurrentPosition);
+	}
 
-	for (LList::Iterator CurrentPosition = l.Begin(); CurrentPosition != l.End(); ++CurrentPosition)
+	for (LList<int>::ReverseIterator CurrentPosition = l.RBegin(); CurrentPosition != l.REnd(); CurrentPosition++)
 	{
 		cout << (*CurrentPosition)->Value << endl;
 	}
+
+	//vector<int> testVec;
+	//list<int> testList;
+
+	//for (int i = 0; i < 10; ++i)
+	//{
+	//	testVec.push_back(i);
+	//	testList.push_back(i);
+	//}
+	//vector<int>::iterator v_iter;
+	//list<int>::iterator l_iter;
+
+	//for (vector<int>::reverse_iterator iter = testVec.rbegin(); iter != testVec.rend(); iter++)
+	//{
+	//	cout << (*iter) << endl;
+	//}
+
+	//for (list<int>::iterator iter = testList.begin(); iter != testList.end(); ++iter)
+	//{
+	//	cout << (*iter) << endl;
+	//}
+
+	//for (int i = 0; i < testVec.size(); ++i)
+	//{
+	//	cout << testVec[i] << endl;
+	//}
+
+	//for (int i = 0; i < testList.size(); ++i)
+	//{
+	//	cout << testVec[i] << endl;
+	//}
+
+
+
 
 	return 0;
 }
